2b7d7d371b2c80e6d218b1dd31892f5fa4ba4154
Lauter Fressen <lauterfressen@gmail.com>
2019-12-22 21:39:35 +0100

    prl

---
+++ specifica/esercitazione progetto reti logiche/square_progetto_vivado/square/square.sim/sim_1/synth/func/xsim/xsim.dir/square_tb_func_synth/obj/xsim_1.c
@@ -0,0 +1,238 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern void execute_476(char*, char *);
+extern void execute_477(char*, char *);
+extern void execute_478(char*, char *);
+extern void execute_26(char*, char *);
+extern void execute_30(char*, char *);
+extern void execute_35(char*, char *);
+extern void execute_39(char*, char *);
+extern void execute_44(char*, char *);
+extern void execute_82(char*, char *);
+extern void execute_84(char*, char *);
+extern void execute_85(char*, char *);
+extern void execute_86(char*, char *);
+extern void execute_116(char*, char *);
+extern void execute_121(char*, char *);
+extern void execute_124(char*, char *);
+extern void execute_210(char*, char *);
+extern void execute_211(char*, char *);
+extern void execute_212(char*, char *);
+extern void execute_213(char*, char *);
+extern void execute_214(char*, char *);
+extern void execute_215(char*, char *);
+extern void execute_216(char*, char *);
+extern void execute_303(char*, char *);
+extern void execute_306(char*, char *);
+extern void execute_311(char*, char *);
+extern void execute_315(char*, char *);
+extern void execute_318(char*, char *);
+extern void execute_323(char*, char *);
+extern void execute_327(char*, char *);
+extern void execute_329(char*, char *);
+extern void execute_332(char*, char *);
+extern void execute_335(char*, char *);
+extern void execute_340(char*, char *);
+extern void execute_375(char*, char *);
+extern void execute_389(char*, char *);
+extern void execute_391(char*, char *);
+extern void execute_392(char*, char *);
+extern void execute_424(char*, char *);
+extern void execute_425(char*, char *);
+extern void execute_428(char*, char *);
+extern void execute_434(char*, char *);
+extern void execute_470(char*, char *);
+extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void transaction_38(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_40(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_41(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_42(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_44(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_46(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_58(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_59(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_60(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_61(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_62(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_63(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_64(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_65(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_66(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_67(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_68(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_69(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_70(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_71(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_72(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_73(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_74(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_75(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_76(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_77(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_78(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_79(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_80(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_81(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_83(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_84(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_85(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_86(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_87(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_88(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_89(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_90(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_91(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_92(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_93(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_94(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_95(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_96(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_97(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_98(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_99(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_100(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_101(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_102(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_103(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_104(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_105(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_106(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_107(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_108(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_109(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_110(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_111(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_112(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_113(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_120(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_124(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_127(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_130(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_137(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_144(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_151(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_158(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_165(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_172(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_179(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_186(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_199(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_206(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_213(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_226(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_233(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_240(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_247(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_254(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_261(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_268(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_281(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_288(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_295(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_302(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_315(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_322(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_329(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_336(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_343(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_350(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_357(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_364(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_371(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_378(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_385(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_392(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[142] = {(funcp)execute_476, (funcp)execute_477, (funcp)execute_478, (funcp)execute_26, (funcp)execute_30, (funcp)execute_35, (funcp)execute_39, (funcp)execute_44, (funcp)execute_82, (funcp)execute_84, (funcp)execute_85, (funcp)execute_86, (funcp)execute_116, (funcp)execute_121, (funcp)execute_124, (funcp)execute_210, (funcp)execute_211, (funcp)execute_212, (funcp)execute_213, (funcp)execute_214, (funcp)execute_215, (funcp)execute_216, (funcp)execute_303, (funcp)execute_306, (funcp)execute_311, (funcp)execute_315, (funcp)execute_318, (funcp)execute_323, (funcp)execute_327, (funcp)execute_329, (funcp)execute_332, (funcp)execute_335, (funcp)execute_340, (funcp)execute_375, (funcp)execute_389, (funcp)execute_391, (funcp)execute_392, (funcp)execute_424, (funcp)execute_425, (funcp)execute_428, (funcp)execute_434, (funcp)execute_470, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_38, (funcp)transaction_40, (funcp)transaction_41, (funcp)transaction_42, (funcp)transaction_44, (funcp)transaction_46, (funcp)transaction_58, (funcp)transaction_59, (funcp)transaction_60, (funcp)transaction_61, (funcp)transaction_62, (funcp)transaction_63, (funcp)transaction_64, (funcp)transaction_65, (funcp)transaction_66, (funcp)transaction_67, (funcp)transaction_68, (funcp)transaction_69, (funcp)transaction_70, (funcp)transaction_71, (funcp)transaction_72, (funcp)transaction_73, (funcp)transaction_74, (funcp)transaction_75, (funcp)transaction_76, (funcp)transaction_77, (funcp)transaction_78, (funcp)transaction_79, (funcp)transaction_80, (funcp)transaction_81, (funcp)transaction_83, (funcp)transaction_84, (funcp)transaction_85, (funcp)transaction_86, (funcp)transaction_87, (funcp)transaction_88, (funcp)transaction_89, (funcp)transaction_90, (funcp)transaction_91, (funcp)transaction_92, (funcp)transaction_93, (funcp)transaction_94, (funcp)transaction_95, (funcp)transaction_96, (funcp)transaction_97, (funcp)transaction_98, (funcp)transaction_99, (funcp)transaction_100, (funcp)transaction_101, (funcp)transaction_102, (funcp)transaction_103, (funcp)transaction_104, (funcp)transaction_105, (funcp)transaction_106, (funcp)transaction_107, (funcp)transaction_108, (funcp)transaction_109, (funcp)transaction_110, (funcp)transaction_111, (funcp)transaction_112, (funcp)transaction_113, (funcp)transaction_120, (funcp)transaction_124, (funcp)transaction_127, (funcp)transaction_130, (funcp)transaction_137, (funcp)transaction_144, (funcp)transaction_151, (funcp)transaction_158, (funcp)transaction_165, (funcp)transaction_172, (funcp)transaction_179, (funcp)transaction_186, (funcp)transaction_199, (funcp)transaction_206, (funcp)transaction_213, (funcp)transaction_226, (funcp)transaction_233, (funcp)transaction_240, (funcp)transaction_247, (funcp)transaction_254, (funcp)transaction_261, (funcp)transaction_268, (funcp)transaction_281, (funcp)transaction_288, (funcp)transaction_295, (funcp)transaction_302, (funcp)transaction_315, (funcp)transaction_322, (funcp)transaction_329, (funcp)transaction_336, (funcp)transaction_343, (funcp)transaction_350, (funcp)transaction_357, (funcp)transaction_364, (funcp)transaction_371, (funcp)transaction_378, (funcp)transaction_385, (funcp)transaction_392};
+const int NumRelocateId= 142;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/square_tb_func_synth/xsim.reloc",  (void **)funcTab, 142);
+	iki_vhdl_file_variable_register(dp + 87120);
+	iki_vhdl_file_variable_register(dp + 87176);
+
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/square_tb_func_synth/xsim.reloc");
+}
+
+void simulate(char *dp)
+{
+	iki_schedule_processes_at_time_zero(dp, "xsim.dir/square_tb_func_synth/xsim.reloc");
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/square_tb_func_synth/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/square_tb_func_synth/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/square_tb_func_synth/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
