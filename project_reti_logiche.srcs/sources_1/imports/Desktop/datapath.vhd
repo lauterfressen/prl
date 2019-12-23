b137b6accbb449500f439f4e90ce4c6a533367fd
Lauter Fressen <lauterfressen@gmail.com>
2019-12-23 15:02:47 +0100

    prl

--- project_reti_logiche.srcs/sources_1/imports/Desktop/datapath.vhd
+++ project_reti_logiche.srcs/sources_1/imports/Desktop/datapath.vhd
@@ -18,7 +18,6 @@ architecture Behavioral of datapath is
 signal o_reg0 : STD_LOGIC_VECTOR (7 downto 0);
 signal o_reg1 : STD_LOGIC_VECTOR (2 downto 0);
 signal sum : STD_LOGIC_VECTOR(2 downto 0);
-signal mux : STD_LOGIC_VECTOR(7 downto 0);
 signal dec : STD_LOGIC_VECTOR(3 downto 0);
 signal sub : STD_LOGIC_VECTOR(7 downto 0);
 begin
@@ -47,9 +46,16 @@ begin
     sum <= o_reg1 + "001";
     
     with d_sel select
-        o_data <= o_reg0(7 downto 0) when '0',
+        o_data <= o_reg0 when '0',
                   '1' & o_reg1 & dec when '1',
                   "XXXXXXXX" when others;
+    
+    with sum(1 downto 0) select
+        dec <= "0001" when "00",
+               "0010" when "01",
+               "0100" when "10",
+               "1000" when "11",
+               "XXXX" when others;
     
     sub <= o_reg0 - i_data;
     
